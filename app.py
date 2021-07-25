33739496795470115b10e29bb4082a50109f302f
Baluds <baluds1999@gmail.com>
2021-07-25 19:20:47 +0530

    Added yolo object detection functionality

--- app.py
+++ app.py
@@ -38,6 +38,58 @@ time.sleep(2.0)
 def basic():
     return render_template("message.html")
 
+def load_yolo():
+	net = cv2.dnn.readNet("yolov3.weights", "yolov3.cfg")
+	classes = []
+	with open("obj.names", "r") as f:
+		classes = [line.strip() for line in f.readlines()]
+	layers_names = net.getLayerNames()
+	output_layers = [layers_names[i[0]-1] for i in net.getUnconnectedOutLayers()]
+	colors = np.random.uniform(0, 255, size=(len(classes), 3))
+	return net, classes, colors, output_layers
+
+def detect_objects(img, net, outputLayers):			
+	blob = cv2.dnn.blobFromImage(img, scalefactor=0.00392, size=(320, 320), mean=(0, 0, 0), swapRB=True, crop=False)
+	net.setInput(blob)
+	outputs = net.forward(outputLayers)
+	return blob, outputs
+
+
+def get_box_dimensions(outputs, height, width):
+	boxes = []
+	confs = []
+	class_ids = []
+	for output in outputs:
+		for detect in output:
+			scores = detect[5:]
+			class_id = np.argmax(scores)
+			conf = scores[class_id]
+			if conf > 0.3:
+				center_x = int(detect[0] * width)
+				center_y = int(detect[1] * height)
+				w = int(detect[2] * width)
+				h = int(detect[3] * height)
+				x = int(center_x - w/2)
+				y = int(center_y - h / 2)
+				boxes.append([x, y, w, h])
+				confs.append(float(conf))
+				class_ids.append(class_id)
+	return boxes, confs, class_ids
+
+def draw_labels(boxes, confs, colors, class_ids, classes, img): 
+	indexes = cv2.dnn.NMSBoxes(boxes, confs, 0.5, 0.4)
+	font = cv2.FONT_HERSHEY_PLAIN
+	for i in range(len(boxes)):
+		if i in indexes:
+			x, y, w, h = boxes[i]
+			label = str(classes[class_ids[i]])
+			color = colors[i]
+			cv2.rectangle(img, (x,y), (x+w, y+h), color, 2)
+			cv2.putText(img, label, (x, y - 5), font, 1, color, 1)
+	img=cv2.resize(img, (800,600))
+	cv2.imshow("Image", img)
+
+
 def rfa():
     global video_capture, outputFrame, lock,crim,criname,cridict
     
@@ -92,6 +144,7 @@ def rfa():
     
     ]
     count=0
+    model, classes, colors, output_layers = load_yolo()
     while True:
     # Grab a single frame of video
         ret, frame = video_capture.read()
@@ -150,7 +203,24 @@ def rfa():
                     #crilist.append(name)
                 #count=0
             #(flag, encodedImage) = cv2.imencode(".jpg", frame)
-    # Display the resulting image  
+        # Display the resulting image
+        height, width, channels = frame.shape
+        blob, outputs = detect_objects(frame, model, output_layers)
+        boxes, confs, class_ids = get_box_dimensions(outputs, height, width)
+        indexes = cv2.dnn.NMSBoxes(boxes, confs, 0.5, 0.4)
+        font = cv2.FONT_HERSHEY_PLAIN
+        for i in range(len(boxes)):
+            if i in indexes:
+                x, y, w, h = boxes[i]
+                label = str(classes[class_ids[i]])
+                try:
+                    color = colors[i]
+                except:
+                    print("error occurred")
+                cv2.rectangle(frame, (x,y), (x+w, y+h), color, 2)
+                cv2.putText(frame, label, (x, y - 5), font, 1, color, 1)
+	    #img=cv2.resize(frame, (800,600))
+	    #cv2.imshow("Image", frame) 
         with lock:
             outputFrame = frame.copy()
 
